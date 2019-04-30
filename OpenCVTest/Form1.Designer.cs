729affd56c7d188ca655bb2ff88c41f7c303b23e
parksodam0328 <parksodam0228@e-mirim.hs.kr>
2019-04-30 11:04:39 +0900

    Template Matching

---
+++ OpenCVTest/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace OpenCVTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Find = new System.Windows.Forms.Button();
+            this.lb_result_text = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 546);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Find
+            // 
+            this.Find.Location = new System.Drawing.Point(12, 574);
+            this.Find.Name = "Find";
+            this.Find.Size = new System.Drawing.Size(776, 36);
+            this.Find.TabIndex = 1;
+            this.Find.Text = "Find";
+            this.Find.UseVisualStyleBackColor = true;
+            this.Find.Click += new System.EventHandler(this.Find_Click);
+            // 
+            // lb_result_text
+            // 
+            this.lb_result_text.AutoSize = true;
+            this.lb_result_text.Font = new System.Drawing.Font("굴림", 12F);
+            this.lb_result_text.Location = new System.Drawing.Point(12, 631);
+            this.lb_result_text.Name = "lb_result_text";
+            this.lb_result_text.Size = new System.Drawing.Size(56, 16);
+            this.lb_result_text.TabIndex = 2;
+            this.lb_result_text.Text = "유사도";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 665);
+            this.Controls.Add(this.lb_result_text);
+            this.Controls.Add(this.Find);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Find;
+        private System.Windows.Forms.Label lb_result_text;
+    }
+}
+
---
+++ OpenCVTest/Form1.cs
@@ -0,0 +1,76 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Point = OpenCvSharp.Point;
+
+namespace OpenCVTest
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Init()
+        {
+            Bitmap bitmapImage = new Bitmap(Application.StartupPath + @"\dogs2.jpg");
+            pictureBox1.Image = bitmapImage;
+            System.Diagnostics.Debug.WriteLine("메시지 "+Application.StartupPath);
+        }
+
+        private void Find_Click(object sender, EventArgs e)
+        {
+            TemplateMatching();
+        }
+
+        private void TemplateMatching() // 템플릿 매칭 : 동일한 크기와 방향을 가진 똑같은 이미지를 가지고 검색할 때 이용. 서로 다른 이미지 유사도 확인 어려움
+        {
+            Mat screen = null, find = null, res = null;
+            try
+            {
+                screen = BitmapConverter.ToMat(new Bitmap(Application.StartupPath + @"\dogs2.jpg"));
+                find = BitmapConverter.ToMat(new Bitmap(Application.StartupPath + @"\dog.jpg"));
+
+                //이미지 유사도 찾기
+
+                res = screen.MatchTemplate(find, TemplateMatchModes.CCoeffNormed);
+
+                double min, max = 0;
+                Point minLoc, maxLoc;
+
+                Cv2.MinMaxLoc(res, out min, out max, out minLoc, out maxLoc);
+                Cv2.Rectangle(screen, maxLoc, new Point(maxLoc.X + find.Width, maxLoc.Y + find.Height), Scalar.Aqua, 3);
+                pictureBox1.Image = BitmapConverter.ToBitmap(screen);
+
+                lb_result_text.Text = max.ToString();
+            }
+            catch (Exception ex)
+            {
+                lb_result_text.Text = ex.Message.ToString();
+            }
+            finally
+            {
+                screen.Dispose();
+                find.Dispose();
+                res.Dispose();
+            }
+        }
+
+    }
+}
